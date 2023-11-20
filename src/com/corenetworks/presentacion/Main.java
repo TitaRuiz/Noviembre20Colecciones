87504f1e213399ce7c18bfd9c5a6aab9903fda0f
TitaRuiz <ggp2002es@gmail.com>
2023-11-20 17:41:33 +0100

    final de clase

--- src/com/corenetworks/presentacion/Main.java
+++ src/com/corenetworks/presentacion/Main.java
@@ -1,5 +1,41 @@
+package com.corenetworks.presentacion;
+
+import com.corenetworks.modelo.Empleado;
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //Probar la interface Set
+        HashSet conjunto = new HashSet(); //Sin usar polimorfismo
+        Set conjunto1 = new HashSet(); //Recomendable utilizar polimorfismo
+        System.out.println("esta vacio ? ->" + conjunto1.isEmpty());
+
+        System.out.println(conjunto1.add("casa"));
+        System.out.println(conjunto1.add("casa"));
+        System.out.println(conjunto1);
+        conjunto1.add("jardin");
+        System.out.println(conjunto1);
+
+        System.out.println("La coleccion contiene el elemento casa ? " + conjunto1.contains("casa"));
+        System.out.println("numero de elementos " +conjunto1.size());
+        conjunto1.add("silla");
+        conjunto1.add("mesa");
+        conjunto1.add(5);
+        System.out.println(conjunto1);
+
+        Empleado e1 = new Empleado(1);
+        conjunto1.add(e1);
+        System.out.println(conjunto1);
+        System.out.println(conjunto1.remove(e1));
+        System.out.println(conjunto1);
+        System.out.println("Recorrer el conjunto ..");
+        for (Object elemento:
+             conjunto1) {
+            System.out.println(elemento.toString());
+        }
+
+
     }
 }
